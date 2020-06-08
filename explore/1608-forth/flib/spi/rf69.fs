80901fe8e557d334cb611c6f8d705086c6a3fbde
dcollins <david.collins@gazprom-mt.com>
2020-06-08 20:07:12 +0100

    Remove len and addr setting in rf-send, update rf-power to use dbm, set config before setting freq, etc so they aren't over written

--- explore/1608-forth/flib/spi/rf69.fs
+++ explore/1608-forth/flib/spi/rf69.fs
@@ -7,7 +7,7 @@
        $00 constant RF:FIFO
        $01 constant RF:OP
        $07 constant RF:FRF
-       $11 constant RF:PA
+       $11 constant RF:PA_LEVEL
        $18 constant RF:LNA
        $1F constant RF:AFC
        $24 constant RF:RSSI
@@ -59,15 +59,12 @@ RF:M_STDBY variable rf.idle-mode  \ default idle mode
      false variable rf.last       \ flag used to fetch RSSI only once per packet
          0 variable rf.rssi       \ RSSI signal strength of last reception
          0 variable rf.lna        \ Low Noise Amplifier setting (set by AGC)
+         0 variable rf.power      \ power setting
          0 variable rf.afc        \ Auto Frequency Control offset
      false variable rf.recvd      \ flag to show packet was received
-      true variable rf.sent       \ flag to show packet can be sent
-         0 variable rf.pkt#       \ length of packet
-     false variable rf.fixed-pkt# \ flag to show fixed packet length
+      true variable rf.can-send   \ flag to show packet can be sent
+         0 variable rf.fixed-pkt# \ flag to show fixed packet length
         66 buffer:  rf.buf        \ buffer with last received packet data
-  rf.buf     constant rf.to_addr
-  rf.buf 1 + constant rf.data
-
 
 8683 variable rf.freq    \ frequency (auto-scaled to 100..999 MHz)
   42 variable rf.group   \ network group (1..250)
@@ -104,8 +101,8 @@ decimal align
 
 : rf-recvd-s! ( -- )  true rf.recvd ! ;
 : rf-recvd-c! ( -- ) false rf.recvd ! ;
-: rf-sent-s! ( -- )   true  rf.sent ! ;
-: rf-sent-c! ( -- )  false  rf.sent ! ;
+: rf-can-send-s! ( -- )   true  rf.can-send ! ;
+: rf-can-send-c! ( -- )  false  rf.can-send ! ;
 
 \ r/w access to the RF registers
 : rf!@ ( b reg -- b ) +spi >spi >spi> -spi ;
@@ -139,6 +136,7 @@ decimal align
   ;
 
 : rf-freq! ( u -- )  \ set the frequency, supports any input precision
+  dup rf.freq !
   begin dup 100000000 < while 10 * repeat
   ( f ) 2 lshift  32000000 11 rshift u/mod nip  \ avoid / use u/ instead
   ( u ) dup 10 rshift  RF:FRF rf!
@@ -148,26 +146,21 @@ decimal align
 : rf-group!  ( u -- ) dup rf.group  ! RF:SYN3 rf! ;  \ set the net group (1..250)
 : rf-nodeid! ( u -- ) dup rf.nodeid ! RF:ADDR rf! ; \ set the filter node id
 
-: rf-correct ( -- ) \ correct the freq based on the AFC measurement of the last packet
-  rf.afc @ 16 lshift 16 arshift 61 *         \ AFC correction applied in Hz
-  2 arshift                                  \ apply 1/4 of measured offset as correction
-  5000 over 0< if negate max else min then   \ don't apply more than 5khz
-  rf.freq @ + dup rf.freq ! rf-freq!         \ apply correction
-  ;
-
-: rf-check ( b -- )  \ check that the register can be accessed over SPI
-  begin  dup RF:SYN1 rf!  RF:SYN1 rf@  over = until
-  drop ;
-
-: rf-pkt# ( -- n )
-  rf.fixed-pkt# @ if
-    rf.pkt# @
+: rf-fixed-pkt! ( -- )                      \ determine fixed packet and size
+  RF:CONF rf@ 7 bit and if
+    RF:PAYLOAD_LEN rf@ rf.fixed-pkt# !
   else
-    RF:FIFO rf@ RF:MAXDATA min
-    dup rf.pkt# !
+    0 rf.fixed-pkt# !
   then
   ;
-: rf-fifo@ ( -- ) rf.buf rf-pkt# rf-n@spi ;
+: rf-pkt#@ ( -- n )                         \ pkt length, fetch from radio if variable
+  rf.fixed-pkt# @ if
+    rf.fixed-pkt# @
+  else
+    RF:FIFO rf@ RF:MAXDATA min
+  then
+  ;
+: rf-fifo@ ( -- ) rf.buf rf-pkt#@ rf-n@spi ;
 
 : rf-irq-exit ( -- ) 1 bit EXTI-PR bis! ;
 : rf-handle-irq ( -- ) 		\ setup interrupt from rf69 -> DI00 -> PB0 (exti0) -> jnz
@@ -176,7 +169,7 @@ decimal align
     RF:IRQ2 rf@ RF:IRQ2_SENT and if
       rf-idle-mode!
       ." sent "
-      rf-sent-s!
+      rf-can-send-s!
       rf-irq-exit
     then
   else
@@ -229,26 +222,34 @@ decimal align
 : rf-info ( -- )  \ display reception parameters as hex string
   rf.freq @ h.4 rf.group @ h.2 rf.rssi @ h.2 rf.lna @ h.2 rf.afc @ h.4 ;
 
+: rf-correct ( -- ) \ correct the freq based on the AFC measurement of the last packet
+  rf.afc @ 16 lshift 16 arshift 61 *         \ AFC correction applied in Hz
+  2 arshift                                  \ apply 1/4 of measured offset as correction
+  5000 over 0< if negate max else min then   \ don't apply more than 5khz
+  rf.freq @ + dup rf.freq ! rf-freq!         \ apply correction
+  ;
+
+: rf-check ( b -- )  \ check that the register can be accessed over SPI
+  begin  dup RF:SYN1 rf!  RF:SYN1 rf@  over = until
+  drop ;
+
 \ --------------------------------------------------
 \   External API
 \ --------------------------------------------------
 
-: rf-init ( conf freq group node -- )  \ init RFM69
+: rf-init ( freq group node conf -- )  \ init RFM69
   spi-init
 
   $AA rf-check  $55 rf-check  \ will hang if there is no radio!
 
+  ( conf )  rf-config!
   ( node )  rf-nodeid!
   ( group ) rf-group!
   ( freq )  rf-freq!
-  ( conf )  rf-config!
 
-  \ determine packet size
-  7 bit RF:CONF rf@ or rf.fixed-pkt# !
-  rf.fixed-pkt# @ if RF:PAYLOAD_LEN rf@ rf.pkt# !  then
-
-  \ setup interrupts for radio
-  \ rf-irq-init
+  rf-fixed-pkt!					\ set fixed packet flag/length
+  rf-irq-init 					\ setup interrupts for radio
+  rf-idle-mode!
   ;
 
 : rf. ( -- )  \ print out all the RF69 registers
@@ -261,34 +262,40 @@ decimal align
     loop
   $10 +loop ;
 
-: rf-power ( n -- )  \ change TX power level (0..31)
-  RF:PA rf@ $E0 and or RF:PA rf! ;
+\ : rf-power ( power -- )  \ change TX power level (0..31)
+\   RF:PA_LEVEL rf@ $E0 and or RF:PA_LEVEL rf! ;
+: rf-low-power ( n -- n ) ( power ) 18 + $1F and 7 bit or ;
+: rf-mid-power ( n -- n ) ( power ) 14 + $1F and 6 bit or 5 bit or ;
+: rf-high-power ( n -- n ) ( power ) 11 + $1F and 6 bit or 5 bit or ;
+: rf-power ( power -- )                        \ change TX power level in dbm
+  dup -18 max
+  dup 13 <= if dup rf-low-power                 \ -18dBm to +13dBm
+  else  dup 18 >= if dup rf-high-power          \ +18dBm to +20dBm - need PA1+PA2
+  else dup rf-mid-power                         \ +14dBm to +17dBm
+  then then
+  ( pa_level ) RF:PA_LEVEL rf!
+  ( power ) rf.power !
+  drop
+  ;
 
 : rf-recv ( -- n )                    \ set rx mode and return if received packet
   rf.mode @ RF:M_TX = if
-    false
-  else
-    rf-rx-mode!
-    %01 6 lshift $25 rf!              \ set trigger for PacketReady on DIO0
-
-    rf.recvd @ not if false else rf-recvd-c! true then
+    false exit
   then
+
+  rf-rx-mode!
+  %01 6 lshift $25 rf!                \ set trigger for PacketReady on DIO0
+
+  rf.recvd @ if rf-recvd-c! true else false then
   ;
 
-: rf-send ( buffer len addr flag -- n )          \ send out one packet for node
-  rf.mode @ RF:M_TX = if
-    \ still sending a packet drop the stack and return
-    drop drop drop drop false
-  else
-    rf-idle-mode!  rf-sent-c!
-
-    ( flag ) if
-      ( len ) dup 2+ RF:FIFO rf!
-      ( addr )       RF:FIFO rf!
-    then
-
-    ( addr count ) rf-n!spi
-    rf-tx-mode!
-    true
+: rf-send ( buffer len -- n )         	\ send out one packet for node
+  rf.mode @ RF:M_TX = if 				\ still sending packet drop stack/return
+    drop drop false exit              	\ still sending, early return
   then
+  rf-idle-mode!  rf-can-send-c!
+
+  ( buffer len ) rf-n!spi
+  rf-tx-mode!
+  true
   ;
