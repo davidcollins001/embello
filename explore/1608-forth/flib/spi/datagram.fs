97f419c81ea99c1f19d2f234518d8312c3f76944
dcollins <david.collins@gazprom-mt.com>
2020-06-25 08:56:33 +0100

    factor out sending code and add sleeps

--- explore/1608-forth/flib/spi/datagram.fs
+++ explore/1608-forth/flib/spi/datagram.fs
@@ -29,7 +29,7 @@ RF:MAXDATA buffer: dg.buf
 \  Internal Helpers
 \ --------------------------------------------------
 
-: yield ( -- ) ;
+: yield ( -- ) sleep ;
 
 : dg-seq@ ( -- n ) dg.seq# @ ;
 : dg-seq! ( n -- ) dg.seq# ! ;
@@ -70,52 +70,54 @@ RF:MAXDATA buffer: dg.buf
   0 dg.flags c!                               \ set flags
   ;
 
-: dg-send-ack ( seq addr -- )
-  1 swap ( addr ) .v dg-set-header
-  dg.seq# @ dg.data c!                        \ copy user data to radio buffer
-  0 dg.data 1+ c!                             \ empty rest of ack data
-  drop \ dg.data c!
-  rf.buf DG:HDR# 1+ rf-send
-  drop
-  ;
-
 : dg-wait-sent ( -- )                         \ wait until the radio can send
   begin rf-sending? while yield repeat
   ;
+: dg-send ( buffer len -- )                   \ try until packet has sent
+  begin 2dup  rf-send not while yield repeat
+  2drop
+  ;
+
+: dg-send-ack ( addr -- )
+  1 swap ( addr ) dg-set-header
+  dg-seq@ dg.data c!                          \ copy user data to radio buffer
+  0 dg.data 1+ c!                             \ empty rest of ack data
+  rf.buf DG:HDR# 1+ dg-send
+  ;
+
+: dg-send-retry ( addr buffer len -- )        \ retry sending
+  DG:RETRIES 0 do
+    2dup dg-send dg-wait-sent
+    2 pick dg-recv-ack if rot ( true ) leave then
+  loop
+  2drop drop
+;
 
 \ --------------------------------------------------
 \   External API
 \ --------------------------------------------------
 
 : dg-send-to ( buffer len addr -- n )         \ send out one packet for node
+  ( swap DG:MAXDATA min swap )                 \ bounds check payload
   2dup
   ( len addr ) dg-set-header
-  -rot ( 62 min ) tuck
-  \ TODO bound check
+  -rot  tuck                                  \ push addr to back and copy len
   ( buffer len ) dg.data swap move            \ copy user data to radio buffer
-  DG:HDR# 1+ +                                \ add header len to packet len
+  DG:HDR# 1+ + rf.buf swap dg-send-retry      \ add header len to packet len
 
-  DG:RETRIES 0 do
-    begin dup rf.buf  swap ( len ) rf-send while yield repeat
-    dg-wait-sent
-
-    over dg-recv-ack if rot ( true ) leave then
-  loop
-  2drop
-  \ TODO return success or failure
   rf-idle-mode!
   ;
 
 : dg-recv ( -- addr n )
-    rf-recv if
-      dg.buf rf-recv-done2
-      dg-seq@ dg.from c@ dg-send-ack
-      dg-wait-sent
-      true
-    else
-      false
-    then
-    ;
+  rf-recv if
+    dg.buf rf-recv-done2
+    dg.from c@ dg-send-ack
+    dg-wait-sent
+    true
+  else
+    false
+  then
+  ;
 
 : dg-show-packet ( -- )
   ." RF69 " rf-info space ." ( " rf.rssi @ . ." )" space
@@ -125,7 +127,6 @@ RF:MAXDATA buffer: dg.buf
     dg.buf c@ 1+
     dup h.2 space ." : "
   then
-  \ 11 debug rf.fixed-pkt# @ .
   0 do dg.buf i + c@ h.2 space loop cr
   ;
 
