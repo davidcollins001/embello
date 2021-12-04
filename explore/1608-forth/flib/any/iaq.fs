e47b1a9d329cbf04688a9427f5f9ef80b3d70e53
davidcollins <davidcollins001@gmail.com>
2021-12-04 00:56:06 +0000

    minor updates for iaq

--- explore/1608-forth/flib/any/iaq.fs
+++ explore/1608-forth/flib/any/iaq.fs
@@ -6,8 +6,11 @@
    3 constant IAQ:RATE
  300 constant IAQ:UPDATE_COUNT
  \ 3 constant IAQ:UPDATE_COUNT
-\ true constant DEBUG
-false constant DEBUG
+2000 constant IAQ:GAS_SEND
+  50 constant IAQ:TEMP_SEND
+\ true variable IAQ:DEBUG
+false variable IAQ:DEBUG
+: DEBUG IAQ:DEBUG @ ;
 
 \ This sets the balance between humidity and gas reading in the
 \ calculation of air_quality_score (25:75, humidity:gas)
@@ -87,8 +90,8 @@ $b variable rf.base_addr
   1 iaq.idle +!                             \ increment  counter
   3 pick dup >r  7 pick dup >r              \ store T and gas in return stack
 
-  ( gas )  iaq.prev_gas @ -  abs 1000 > swap
-  ( temp ) iaq.prev_temp @ - abs 50 >
+  ( gas )  iaq.prev_gas @ -  abs IAQ:GAS_SEND > swap
+  ( temp ) iaq.prev_temp @ - abs IAQ:TEMP_SEND >
   iaq.idle @ IAQ:UPDATE_COUNT mod 0=  or or
 
   \ if sending update store current values and reset counter
@@ -97,6 +100,7 @@ $b variable rf.base_addr
   ;
 : iaq-show-data ( n -- )                        \ g h p t tadc iaq iaq2
   cr ." iaq2 iaq t_adc t p h g " ( n ) 0 do i pick . loop
+  ." s: " depth
   ;
 : iaq-send-packet ( g h p t tadc iaq iaq2 -- )
   hwid <pkt  7 0 do +pkt loop  pkt>
@@ -105,12 +109,14 @@ $b variable rf.base_addr
   ;
 : iaq-if-send ( g h p t tadc iaq iaq2 -- )
   \ send packet every degree temp change or UPDATE/RATE secs
+  led-on
   iaq-send-update? if
     DEBUG if 7 iaq-show-data then
     iaq-send-packet
   else
     7 iaq-drop-stack
   then
+  led-off
   ;
 
 : highz-gpio
@@ -158,7 +164,7 @@ $b variable rf.base_addr
   adc-init
   ;
 
-: iaq-sensor-init ( -- )
+: iaq-init ( -- )
   2.1MHz  1000 systick-hz  lptim-init iaq-adc-init highz-gpio
 
   \ configure important RF69 registers
@@ -182,7 +188,7 @@ task: run-iaq
 
 : main ( -- )
   \ run-iaq activate
-  iaq-sensor-init
+  iaq-init
 
   begin
     iaq-gas-ref
@@ -191,13 +197,11 @@ task: run-iaq
       bme-sensor> bme-heat-stable? and if
         3 pick ( gas ) iaq-gas-ref+
 
-        led-off
         adc-temp                              ( stack: g h p t tadc )
         4 pick ( gas ) 4 pick ( hum1000 ) iaq-score
         5 pick ( gas ) 5 pick ( hum1000 ) iaq-score2 1000 /
         iaq-if-send                           ( stack: g h p t tadc iaq iaq2 )
         low-power-sleep
-        led-on
         key? if leave then
       then
     loop
